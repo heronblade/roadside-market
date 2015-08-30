2acb7ebfab3a364beecc64603b0eee3e008fb090
Joshua Byler <bylerj@gmail.com>
2015-08-30 13:54:29 -0400

    For the JS - added event listeners for the various sign in/out templates For the CSS - centered the h2 on the signup page For the HTML - added id to sign out button so i could key off of it with the javascript.  Also built up the forms for the sign in/out/up pages

--- client/main.js
+++ client/main.js
@@ -1,8 +1,6 @@
 Template.layout.onRendered(function () {
   $('.button-collapse').sideNav();
 
-
-
   // smooth scroll to anchor points
 	$('a[href*=#]:not([href=#])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -18,8 +16,56 @@ Template.layout.onRendered(function () {
 	});
 });
 
+Template.layout.events({
+  'click #sign-out': function(e, t) {
+    e.preventDefault();
+    Meteor.logout();
+  }
+});
+
 Template.home.onRendered(function () {
   	setBanner();
+});
+
+Template.signIn.events({
+  'submit #sign-in-form': function(e, t) {
+    e.preventDefault();
+    console.log(t);
+    var email = t.find('#login-email').value,
+    password = t.find('#login-password').value;
+
+    Meteor.loginWithPassword(email, password, function(err) {
+      if (err) {
+        console.log('error');
+        console.warn(err);
+      } else {
+        console.log('you did it');
+      }
+    });
+  }
+});
+
+Template.signUp.events({
+  'submit #sign-up-form': function(e, t) {
+    e.preventDefault();
+    var firstName = t.find('#first-name').value,
+    lastName = t.find('#last-name').value,
+    password = t.find('#login-password').value,
+    email = t.find('#login-email').value;
+
+    Accounts.createUser({
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName
+    }, function(err) {
+      if (err) {
+        console.warn(err);
+      } else {
+        console.log('you signed up');
+      }
+    });
+  }
 });
 
 // dynamically set the banner-area height
